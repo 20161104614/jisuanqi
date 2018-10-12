f0738b4a14d3206dcb0fd2276371477e7c6778af
20161104614 <964829629@qq.com>
2018-10-12 17:43:12 +0800

    2
    2

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -11,8 +11,78 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var show: UITextField!
+    @IBOutlet weak var result_1: UITextField!
+    @IBOutlet weak var result_2: UITextField!
     
     var userIsInTheMiddleOfTypingANumber: Bool = false
+    var number = 0//判断加减乘除
+    var judge = 0//决定输出数字的位数
+    var add = 0
+    var re = 0//判断result.text前是否存在符号
+    
+    @IBAction func add(_ sender: Any) {
+        if add == 1{
+            let a = Double(result_1.text!)!
+            let b = Double(show.text!)!
+            let c = a + b
+            result_1.text = String(c)
+            show.text = "+"
+            number = 2
+            re = 1
+            }
+            else{
+            if show.text == ""{
+                show.text = "0"
+                }
+                else {
+                let x = Double(result_1.text!)!
+                result_1.text = String(x)
+                show.text = "+"
+                number = 2
+                re = 0
+                }
+            }
+    }
+    @IBAction func minus(_ sender: Any) {
+    }
+    @IBAction func multiply(_ sender: Any) {
+    }
+    @IBAction func divide(_ sender: Any) {
+    }
+    @IBAction func equal(_ sender: Any) {
+        var d:Double
+        var c:Double
+        let x = Double(result_1.text!)!
+        c = (show.text! as NSString).doubleValue
+        if number == 1 {
+        d = x - c
+        }else if number == 2 {
+        d = x + c
+        }else if number == 3 {
+        d = x * c
+        }else if number == 4 {
+        d = x / (c)
+        }else {
+        d = 1000
+        }
+        result_2.text = String(c)
+        if judge == 1{
+            show.text = String(format:"%f", d)
+        }
+        else {
+            show.text = String(format:"%.0f", d)
+             }
+             re = 1
+             judge = 0
+             add = 0
+    }
+    @IBAction func change(_ sender: Any) {
+    }
+    @IBAction func percent(_ sender: Any) {
+    }
+    @IBAction func entrt(_ sender: Any) {
+        show.text = ""
+    }
     
     @IBAction func num_1(_ sender: Any) {
         if userIsInTheMiddleOfTypingANumber{
@@ -105,12 +175,10 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    @IBAction func entrt(_ sender: Any) {
-       show.text = ""
+    @IBAction func dot(_ sender: Any) {
+        show.text = show.text! + "."
+        judge = 1
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
